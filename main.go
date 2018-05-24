7528ffa75d30d0b83d6943c3089c8e37f9f73112
Scott Jones <scottdj92@gmail.com>
2018-05-23 20:56:32 -0400

    Change port to make it easier for local dev

--- main.go
+++ main.go
@@ -14,8 +14,8 @@ func main() {
 	router.HandleFunc("/note/{id}", crud.GetNote).Methods("GET")
 	router.HandleFunc("/note/{id}", crud.CreateNote).Methods("POST")
 
-	log.Println("server is running at port 8080")
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	log.Println("server is running at port 1337")
+	if err := http.ListenAndServe(":1337", router); err != nil {
 		panic(err)
 	}
 }
